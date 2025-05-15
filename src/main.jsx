8921f765e5bde092b81c9cc807482b0cb9579c34
Kenovel <kenovel174@gmail.com>
2025-05-15 20:41:54 +0500

    Реализован основной компонент блога с роутингом

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Blog from './Blog';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
+    <BrowserRouter>
+        <Blog />
+    </BrowserRouter>,
 );
